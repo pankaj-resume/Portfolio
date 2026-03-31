5d21be59370a8ccc862c5cecfafadf771694c193
Pankaj deka <deka39999@gmail.com>
2026-03-31 14:57:41 +0530

    update

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ function scrollLeft() {
   container.scrollBy({ left: -300, behavior: 'smooth' });
 }
 
+const roles = ["AI Developer", "Full Stack Developer", "Graphic Designer"];
+
+let i = 0, j = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  let current = roles[i];
+
+  if (isDeleting) {
+    j--;
+  } else {
+    j++;
+  }
+
+  document.getElementById("typing").innerText = current.substring(0, j);
+
+  if (!isDeleting && j === current.length) {
+    isDeleting = true;
+    setTimeout(typeEffect, 1200);
+    return;
+  }
+
+  if (isDeleting && j === 0) {
+    isDeleting = false;
+    i = (i + 1) % roles.length;
+  }
+
+  setTimeout(typeEffect, isDeleting ? 50 : 100);
+}
+
+typeEffect();
